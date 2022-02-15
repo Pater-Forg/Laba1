ad6f38ec1394f1f5cdae5cea1ab2658f433d0f13
Pater-Forg <gbogdanovg@gmail.com>
2022-02-15 14:13:27 +0300

    1 лабораторная завершена

---
+++ lab1/Program.cs
@@ -0,0 +1,26 @@
+﻿var rand = new Random();
+Console.WriteLine("Введите число:");
+try
+{
+    var n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"{n} + 32 = {n+32}");
+    Console.WriteLine($"{n}^3 = {Math.Pow(n, 3)}");
+    Console.WriteLine($"|{n}| = {Math.Abs(n)}");
+    Console.WriteLine($"sqrt({n}) = {Math.Sqrt(n)}");
+
+    var arr = new float[n];
+    for (var i = 0; i < n; i++)
+    {
+        arr[i] = rand.NextSingle();
+    }
+    Console.WriteLine($"Случайный массив размерностью {n}:");
+    foreach (var elem in arr)
+    {
+        Console.Write($"{elem} ");
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
